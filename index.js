0c3afde5d5b8e38f4a087805fb53cffc93a02655
festus wanjohi <strategic.policyafrica@gmail.com>
2022-08-08 23:43:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+const employee = {
+  name: "festus",
+  streetAddress: "223 Nairobi",
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return { ...obj, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const myObj = { ...employee };
+  delete myObj[key];
+  return myObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
